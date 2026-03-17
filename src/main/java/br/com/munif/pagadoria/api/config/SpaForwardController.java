54d2ef2e3628232061d6f4f93cc59e0bb2b39ea9
munif <munifgebara@gmail.com>
2026-03-17 16:27:22 -0300

    Ajustes no spring e configurações.

--- src/main/java/br/com/munif/pagadoria/api/config/SpaForwardController.java
+++ src/main/java/br/com/munif/pagadoria/api/config/SpaForwardController.java
@@ -3,16 +3,12 @@ package br.com.munif.pagadoria.api.config;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
 @Controller
 public class SpaForwardController {
 
-    @GetMapping({
-            "/app",
-            "/app/",
-            "/app/{path:[^\\.]*}",
-            "/app/**/{path:[^\\.]*}"
-    })
-    public String forward() {
+    @GetMapping("/app")
+    public String forwardApp() {
         return "forward:/app/index.html";
     }
 }
